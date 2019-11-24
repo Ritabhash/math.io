e7d6ba97e17468024205bc924419b683caf06744
Ritabhash Das <31769849+Ritabhash@users.noreply.github.com>
2019-11-24 18:01:22 +0530

    Add files via upload

---
+++ math.io/quiz.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+import numpy as np
+import json
+
+no_questions = 5
+data = []
+with open("data/data.json") as f:
+    for line in f:
+        j_content = json.loads(line)
+        data.append(j_content)
+        
+def select_questions():
+    problems = np.random.choice(data, no_questions , replace=False)
+    prob_dict = {}
+    for i in range(0,no_questions):
+        prob_dict[str(i)] = problems[i]
+    return prob_dict
+
+
+def sample_questions():
+    problems = np.random.choice(data, no_questions , replace=False)
+    prob_dict = []
+    for i in range(0,no_questions):
+        prob_dict.append(problems[i])
+    return prob_dict
+#select_questions()
